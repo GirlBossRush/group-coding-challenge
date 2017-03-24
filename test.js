7ab670d2fe48ec80780fe3022b13eec2aeb5c9b7
J.D. Sandifer <sandifer.jd@gmail.com>
2017-03-23 22:10:35 -0700

    Refactor test.js for better naming, style
    Added more descriptive names for some tests to better describe the
    function's purpose. Added falsifiable descriptions for each assert so
    it's clear what should be happening. Replaced most assert.ok()'s with
    assert.srictEqual() to better describe the test in the output - actual
    expected/result values instead of true/false. (Except I left one
    assert.ok() to be refactored by the group.) Added more tests to fizzBuzz
    to make a good first example.

--- test.js
+++ test.js
@@ -1,25 +1,48 @@
-QUnit.test("Sanity Check 1", function(assert) {
-  assert.ok(1 < 2, "Passed!")
+QUnit.test("Sanity Check (testing the test framework)", function(assert) {
+  assert.notStrictEqual(1, 2, "1 does not equal 2. As it should be!")
 })
 
 QUnit.test("Fizz Buzz", function(assert) {
-  assert.ok(fizzBuzz(3) === "fizz", "Multiples of 3")
-  assert.ok(fizzBuzz(5) === "buzz", "Multiples of 3")
-  assert.ok(fizzBuzz(15) === "fizzbuzz", "Multiples of 3 and 5")
+  assert.strictEqual(
+      fizzBuzz(3),
+      "fizz",
+      "Multiples of 3 return 'fizz'.")
+  assert.strictEqual(
+      fizzBuzz(5),
+      "buzz",
+      "Multiples of 5 return 'buzz'.")
+  assert.strictEqual(
+      fizzBuzz(15),
+      "fizzbuzz",
+      "Multiples of 3 and 5 return 'fizzbuzz'.")
+  assert.strictEqual(
+      fizzBuzz(11),
+      11,
+      "Otherwise, the given number is returned.")
 })
 
-QUnit.test("Reverse Sentence", function(assert) {
-  assert.ok(reverseSentence("bob likes dogs") === "dogs like bob", "Passed!")
+QUnit.test("Reverse Word Order In A Sentence", function(assert) {
+  assert.ok(reverseSentence("bob likes dogs") === "dogs like bob",
+      "Three word sentence is reordered in reverse.")
 })
 
-QUnit.test("Duplicate", function(assert) {
-  assert.deepEqual(duplicate([1,2,3,4,5]), [1,1,2,2,3,3,4,4,5,5], "Passed!")
+QUnit.test("Duplicate Items In An Array", function(assert) {
+  assert.deepEqual(
+      duplicate([1,2,3,4,5]),
+      [1,1,2,2,3,3,4,4,5,5],
+      "Ordered array of numbers 1-5 is duplicated.")
 })
 
 QUnit.test("Implement String.prototype.split", function(assert) {
-  assert.deepEqual(split("foo bar baz"), ["foo", "bar", "baz"], "Passed!")
+  assert.deepEqual(
+      split("foo bar baz"),
+      ["foo", "bar", "baz"],
+      "Three word sentence is split into an array of three words.")
 })
 
 QUnit.test("Implement Array.prototype.join", function(assert) {
-  assert.deepEqual(join(["foo", "bar", "baz"], " "), "foo bar baz", "Passed!")
+  assert.deepEqual(
+      join(["foo", "bar", "baz"], " "),
+      "foo bar baz",
+      "Array of three words is joined into three word sentence.")
 })
